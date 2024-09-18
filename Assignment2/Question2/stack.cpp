4174dd5144717441e75268da23daf898d1f5c14b
Atlas Mallams <131003995+atlaska826@users.noreply.github.com>
2024-09-18 13:19:01 -0500

    Added stack files

---
+++ Assignment2/Question2/stack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Atlas Mallams on 9/18/24.
+//
+
+#include "stack.h"
---
+++ Assignment2/Question2/stack.h
@@ -0,0 +1,16 @@
+//
+// Created by Atlas Mallams on 9/18/24.
+//
+
+#ifndef CS_303_ASSIGNMENTS_STACK_H
+#define CS_303_ASSIGNMENTS_STACK_H
+
+
+
+class stack {
+
+};
+
+
+
+#endif //CS_303_ASSIGNMENTS_STACK_H
